395efd706ce2bd7d440ee3b67c2e7605837acebd
Bryan Yee <yee.bryan.c@gmail.com>
2016-09-22 00:14:20 -0700

    Modify server routes

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './')));
-app.use(express.static(path.join(__dirname, '/client/')));
+// app.use(express.static('client'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+})
+
+app.get(/.(js|html|css)$/, (req, res, next) => {
+  console.log(req.url)
+  res.sendFile(path.join(__dirname, 'client', req.url), (err) => {
+    if(err) res.sendStatus(err.status);
+  });
+})
 
 app.use((req, res) => {
   res.sendStatus(404);
